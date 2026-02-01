dbfb34cf897c93c3b27f7955c4f45b7f08864aa2
CarlosSaiz90 <k4rlos_90@hotmail.com>
2026-02-01 20:56:38 +0100

    ok

--- Primer Proyecto/src/Main.java
+++ Primer Proyecto/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
 
+    public static void main (String[]args){
+        System.out.println("hola este es mi primer progama en java");
+        System.out.println();{
 
 
-}
+
+
+    }
+
+aaa
+
+    }
+
+
