7ed2bee829040bb46d2c2150039ac06977a72701
CarlosSaiz90 <k4rlos_90@hotmail.com>
2026-02-13 13:22:14 +0100

    ok

--- ProyectoMetodos/src/GestorFinancieroJava.java
+++ ProyectoMetodos/src/GestorFinancieroJava.java
@@ -11,9 +11,24 @@ public void cambioDolares(double euros){
 
 }
 
-public void mediaNotasCurso(int nota1, int nota2, int nota3){
+public static void mediaNotasCurso(int nota1, int nota2, int nota3){
     int resultado = (nota1+nota2+nota3)/3;
+    System.out.println();
     System.out.println("La media de tus notas es "+resultado);
+}
+
+public static void calcularGastoViaje(double km, double consumoMedio, double precioLitro){
+    double litrosTotales = (km*consumoMedio)/100;
+    double costeFinal = litrosTotales*precioLitro;
+    System.out.printf("El viaje nos ha costado %.2f euros",costeFinal);
+}
+
+public static void convertirCelsiusFahrenheit(double celsius){
+    double resultado = (celsius*9/5)+32;
+    System.out.printf(+ celsius +" grados celsius equivalen a %.1f ",resultado, " farenheit");
+
+
+
 }
 
 
--- ProyectoMetodos/src/Main.java
+++ ProyectoMetodos/src/Main.java
@@ -4,12 +4,25 @@ public class Main {
 
 
         GestorFinancieroJava gestor = new GestorFinancieroJava();
-        GestorFinancieroJava gestor = new GestorFinancieroJava();
-        GestorFinancieroJava gestor = new GestorFinancieroJava();
 
         gestor.calcularIva(250);
         gestor.cambioDolares(1200);
         gestor.mediaNotasCurso(5, 6, 9);
+
+
+        GestorFinancieroJava.mediaNotasCurso(1,7,8);
+        System.out.println();
+        GestorFinancieroJava.convertirCelsiusFahrenheit(22);
+        System.out.println();
+
+
+
+        GestorFinancieroJava.calcularGastoViaje(300,6,1.45);
+
+
+
+
+
 
 
     }
